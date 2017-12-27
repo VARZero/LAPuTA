0f38cb451688cbb4828480b85dc3e7187bb4693c
VARZero <cydiazero@gmail.com>
2017-12-28 00:53:18 +0900

    함수명 변경.

--- LAPuTA_Change.py
+++ LAPuTA_Change.py
@@ -2,9 +2,13 @@
 # * WRITEN BY VARZERO                             *
 # * DATE 2017/12/21                               *
 # * AR디바이스 UI 및 음성인식, 사물인식의 메인             *
+# * LA_Ch_*()                                     *
 # *************************************************
 
 import cv2
 
-def ColorGray(img):
-    cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def LA_Ch_ImageInit(img):
+    return cv2.imread(img)
+
+def LA_Ch_ColorGray(img):
+    return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
